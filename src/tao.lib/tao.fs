58024cd15164554e726e2a51b68ad95f45cd5435
i-ght <toph.ght@gmail.com>
2024-03-17 12:14:37 -0600

    initial

---
+++ src/tao.lib/tao.fs
@@ -0,0 +1,42 @@
+namespace Tao.Lib
+
+open System.Text.RegularExpressions
+
+open Tao.Lib.EnglishTexts
+
+[<Struct>]
+type Tao =
+    { Slice: int
+      Lines: string list }
+
+module Tao =
+
+    let private taoSliceRegex =
+        Regex(
+            "(\d+)\n\n(.*?)\n\nBack to Table of Contents", 
+            RegexOptions.IgnoreCase 
+            ||| RegexOptions.Compiled
+            ||| RegexOptions.Singleline
+        )
+    let constructTao (tao: string) =
+        let sliceMatches = taoSliceRegex.Matches(tao)
+
+        let rec processMatches (matches: Match list) index acc =
+            match matches with
+            | [] -> List.rev acc
+            | head :: tail ->
+                let _id = head.Groups.[1].Value
+                let text = head.Groups.[2].Value
+                let lines =
+                    text.Split("\n")
+                    |> Array.mapi (fun i line -> $"{i}. {line}")
+                let index = index + 1
+                let newSlice = { Slice = index; Lines = List.ofArray lines }
+                let newAcc = newSlice :: acc
+                processMatches tail index newAcc
+
+        processMatches (List.ofSeq sliceMatches) 0 []
+
+    let construct () =
+        [ constructTao GiaFuFenAndJaneEnglish.text; 
+        constructTao CharlesMuller.text ]
---
+++ src/tao/program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+open Tao.Lib
+
+
+let taoSlices = Tao.construct()
+let r = Random()
+
+
+let spinTaoWheel () =
+    let index = r.Next(0, List.length taoSlices[0])
+    let slice = taoSlices[0][index]
+    slice
+
+let taoSlice = spinTaoWheel ()
+printfn "%A" taoSlice
+
