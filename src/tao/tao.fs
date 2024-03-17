977ba5c25f070f7f0a2c8c31397a9b4b899dd104
i-ght <toph.ght@gmail.com>
2024-03-17 17:52:09 -0600

    upgradez

--- src/tao/tao.fs
+++ src/tao/tao.fs
@@ -18,25 +18,25 @@ module Tao =
             ||| RegexOptions.Compiled
             ||| RegexOptions.Singleline
         )
-    let constructTao (tao: string) =
+    let private slice (tao: string) =
         let sliceMatches = taoSliceRegex.Matches(tao)
 
-        let rec processMatches (matches: Match list) index acc =
+        let rec procMatches (matches: Match list) index acc =
             match matches with
             | [] -> List.rev acc
             | head :: tail ->
-                let _id = head.Groups.[1].Value
-                let text = head.Groups.[2].Value
+                let _id = head.Groups[1].Value
+                let text = head.Groups[2].Value
                 let lines =
                     text.Split("\n")
                     |> Array.mapi (fun i line -> $"{i}. {line}")
                 let index = index + 1
-                let newSlice = { UniqueIdentifier = index; Text = List.ofArray lines }
-                let newAcc = newSlice :: acc
-                processMatches tail index newAcc
+                let tao = { UniqueIdentifier = index; Text = List.ofArray lines }
+                let taos = tao :: acc
+                procMatches tail index taos
 
-        processMatches (List.ofSeq sliceMatches) 0 []
+        procMatches (List.ofSeq sliceMatches) 0 []
 
     let construct () =
-        [ constructTao GiaFuFenAndJaneEnglish.text; 
-        constructTao CharlesMuller.text ]
+        [ slice GiaFuFenAndJaneEnglish.text; 
+          slice CharlesMuller.text ]
